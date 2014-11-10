4712d5cfd6e5ac4df7bd912a7de34b40b9cdb021
TheLoutre <rognant.pierre@gmail.com>
2014-11-10 10:38:09 -0500

    mpi init dirty fix

--- pyipoptpackage/__init__.py
+++ pyipoptpackage/__init__.py
@@ -16,8 +16,14 @@ from ipoptconst import *
 from pyipoptcore import *
 from ipoptunconstrained import fmin_unconstrained
 
+from ctypes import *
+mpi = CDLL('libmpi.so.1', RTLD_GLOBAL)
+f = pythonapi.Py_GetArgcArgv
+argc = c_int()
+argv = POINTER(c_char_p)()
+f(byref(argc), byref(argv))
+mpi.MPI_Init(byref(argc), byref(argv))
+
 # verbose messages from the C interface
-import ctypes
-ctypes.CDLL('libmpi.so.1', ctypes.RTLD_GLOBAL)
 set_loglevel(2)
 
